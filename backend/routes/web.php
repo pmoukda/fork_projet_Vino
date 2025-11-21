ca97d183e3cf84bf028fa84c095ee4eeb2759fa6
= <chanmichou37@gmail.com>
2025-11-21 11:31:17 -0500

    Modifs

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,32 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// routes/web.php
-//Route::get('/scrape-saq', [ScraperController::class, 'scrapeAndSave']);
-
-//Route::get('/scrape-saq', [ScraperController::class, 'ScrapeWines']);
-Route::get('/wines', [WineController::class, 'index'])->name('wines.index');
-Route::get('/scrape', [ScraperController::class, 'scrape']);
-
 //Route::get('/scrape-all', [ScraperController::class, 'scrapeAll']);
 Route::get('/scrape-test', [ScraperController::class, 'scrapeTest']);
 
-/*Route::get('/test-scraping', function () {
-    // Lire le fichier HTML sauvegardé
-    $html = file_get_contents(storage_path('app/test.html'));
 
-    // Extraire le JSON-LD
-    preg_match('/<script type="application\/ld\+json">(.*?)<\/script>/s', $html, $jsonMatch);
-    $data = json_decode($jsonMatch[1] ?? '{}', true);
 
-    // Construire le tableau produit
-    $products = [
-        'name' => $data['name'] ?? null,
-        'price' => $data['offers']['price'] ?? null,
-        'country' => $data['additionalProperty'][0]['value'] ?? null,
-        'vintage' => $data['additionalProperty'][1]['value'] ?? null,
-        'image' => $data['image'] ?? null,
-    ];
-
-    return response()->json($products);
-});*/
