7532b1d3682c5146d501e4e0c94158dd5d437128
= <chanmichou37@gmail.com>
2025-12-06 14:30:48 -0500

    Modification et ajout des modales ajouter et erreur

--- frontend/src/pages/AjouterProduitCellier.jsx
+++ frontend/src/pages/AjouterProduitCellier.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../api/axios";
+import ModaleAjouter from "../components/ModalAjouter";
+import ModaleErreur from "../components/ModalErreur";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Fonction fléchée qui affiche les détails d'un vin et qui affiche le formulaire d'ajout du vin sélectionné et qui permet d'ajouter le vin dans le cellier de l'utilisateur connecté. La quantité ajoutée est enregistrée dans la table pivot cellier_produit dans la colonne "quantite"
@@ -8,12 +11,17 @@ import api from "../api/axios";
  * @returns Retourne la fiche de détail d'un vin
  */
  const AjouterProduitCellier = () => {
-    const { id } = useParams();
+    const { produitId } = useParams();
     const [produit, setProduit] = useState(null);
     const [celliers, setCelliers] = useState([]);
     const [cellierSelectionne, setCellierSelectionne] = useState('');
     const [quantite, setQuantite] = useState(1); // valeur par défaut 1
-    const { userId, produitId } = useParams();    
+    const [quantiteAjoutee, setQuantiteAjoutee] = useState(0);
+    const [modalAjouterVisible, setModalAjouterVisible] = useState(false); // affiche la boite modale d'ajout et quantité
+    const [modalErreurVisible, setModalErreurVisible] = useState(false);
+    const [messageAjout, setMessageAjout] = useState("");
+    const [messageErreur, setMessageErreur] = useState("");
+    const navigate = useNavigate();
    
     useEffect(() => {
         api.get("/celliers")
@@ -35,6 +43,8 @@ import api from "../api/axios";
             .then(res => setProduit(res.data))
             .catch(err => console.error("Erreur produit:", err));
     }, [produitId]);
+    
+    
    
     /**
      * Fonction qui ajoute un vin dans un cellier à partir d'un formulaire d'ajoute. Possibilité d'incrémenter ou décrémenter la quantité avant de soumettre.
@@ -43,17 +53,26 @@ import api from "../api/axios";
     const ajouterProduit = () => {
         if (!produit || !cellierSelectionne) return;
 
-        api.post(`/user/${userId}/celliers/${cellierSelectionne}/produits`, {
+        api.post(`/celliers/${cellierSelectionne}/produits`, {
             produit_id: produit.id,
             quantite: quantite
         })
-        .then(() => {
-            alert(`Ajouté ${quantite} bouteille(s) au cellier !`);
-            setQuantite(1); // reset à 1 après ajout
+        .then((res) => {
+            setQuantiteAjoutee(quantite);
+            setMessageErreur(`<p>Vous avez ajouté <strong>${quantiteAjoutee}</strong> bouteille${quantiteAjoutee > 1 ? "s" : ""}.</p>`);
+            setModalAjouterVisible(true);
+            setQuantite(1);
+
+            // fermer la modale après 5s et rediriger vers le cellier
+            setTimeout(() => {
+                setModalAjouterVisible(false);
+                navigate(`/celliers/${cellierSelectionne}`);
+            }, 5000);
+
         })
         .catch(err => {
-            console.error(err);
-            alert("Erreur lors de l'ajout du produit.");
+            setMessageErreur("Erreur lors de l'ajout du produit.");
+            setModalErreurVisible(true);
         });
     };
     {/* Animations 3 points pour le chargement de la page */}
@@ -61,7 +80,7 @@ import api from "../api/axios";
         <span></span><span></span><span></span>
     </div>;
     return (
-        <div className="w-full min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
+        <div className="w-full min-h-screen flex flex-col items-center justify-center mb-[-50px] px-4 sm:px-6 lg:px-8">
             <h1 className="text-4xl mt-10 mb-10">Ajout de bouteilles au cellier</h1>            
             <div className="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl p-4 bg-white rounded-lg shadow-md flex flex-col">                
                 <div className="carteColonne flex flex-col items-center">
@@ -76,14 +95,14 @@ import api from "../api/axios";
                     
                     onChange={e => setCellierSelectionne(e.target.value)} defaultValue={''}
                     >
-                        <option value=""  >Sélectionne ton cellier</option>
+                        <option className="" value=""  >Sélectionne ton cellier</option>
                         {Array.isArray(celliers) && celliers.length > 0 ? (
                             celliers.map(c => <option key={c.id} value={c.id}>{c.nom}</option>)
                         ) : (
                             <option disabled>Aucun cellier</option>
                         )}
                     </select>
-                    <div className="flex w-full gap-2 mb-4">
+                    <div className="flex w-full gap-2 ">
                         <button
                         onClick={() => setQuantite(q => Math.max(0, q - 1))}
                         className="text-xl text-center w-40 font-bold px-4 py-2 rounded-lg bouton-raisin cursor-pointer border-white border-[2px]"
@@ -113,10 +132,22 @@ import api from "../api/axios";
                         onClick={ajouterProduit}
                         className="w-full p-4 bouton-accent text-lg sm:text-xl text-white rounded cursor-pointer border-xs transition-colors"
                     >
-                        Ajouter une bouteille
+                        Ajouter
                     </button>
                 </div>
             </div>
+            {/* Modal ajouté succès et quantité */}
+                <ModaleAjouter
+                    visible={modalAjouterVisible}
+                    quantite={quantiteAjoutee}
+                    message={messageAjout}
+                    onFermer={() => setModalAjouterVisible(false)}
+                />
+                <ModaleErreur
+                    visible={modalErreurVisible}
+                    message={messageErreur}
+                    onFermer={() => setModalErreurVisible(false)}
+                />
         </div>
     );
 }
